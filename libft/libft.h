60568c3cee40845b7475fb509f1e1a85631d59d6
marioguzzzman <marioguzzman@gmail.com>
2026-05-20 12:27:45 +0200

    ft_substr done

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: maguzman <maguzman@student.42.fr>         #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/04/29 22:16:44 by maguzman         #+#    #+#              */
-/*   Updated: 2026/05/19 16:18:23 by maguzman        ###   ########.fr        */
+/*   Updated: 2026/05/20 10:59:03 by maguzman        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,4 +40,6 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
 #endif

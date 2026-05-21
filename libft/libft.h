75fb353f5597443f39898205a6e1656e553493f6
marioguzzzman <marioguzzman@gmail.com>
2026-05-21 17:21:26 +0200

    ft_lstsize done

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: maguzman <maguzman@student.42.fr>         #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/04/29 22:16:44 by maguzman         #+#    #+#              */
-/*   Updated: 2026/05/21 16:37:12 by maguzman        ###   ########.fr        */
+/*   Updated: 2026/05/21 17:09:56 by maguzman        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ typedef struct s_list
 
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);

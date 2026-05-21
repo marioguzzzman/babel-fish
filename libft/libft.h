596608e79b07a5d2e60df096c2bc19e17eef64a6
marioguzzzman <marioguzzman@gmail.com>
2026-05-21 18:00:52 +0200

    ft_lstadd_back done

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: maguzman <maguzman@student.42.fr>         #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/04/29 22:16:44 by maguzman         #+#    #+#              */
-/*   Updated: 2026/05/21 17:22:56 by maguzman        ###   ########.fr        */
+/*   Updated: 2026/05/21 17:36:34 by maguzman        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);

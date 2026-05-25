48199253645bbb4baca39977a5da4ead0a072b40
Mario Alberto Guzman Cerdio <maguzman@c3a1c5.42berlin.de>
2026-05-25 12:53:01 +0200

    strmapi done

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -1,19 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                       :::      ::::::::    */
-/*   ft_strmapi.c                                      :+:      :+:    :+:    */
-/*                                                   +:+ +:+         +:+      */
-/*   By: maguzman <maguzman@student.42.fr>         #+#  +:+       +#+         */
-/*                                               +#+#+#+#+#+   +#+            */
-/*   Created: 2026/05/24 23:31:10 by maguzman         #+#    #+#              */
-/*   Updated: 2026/05/24 23:42:57 by maguzman        ###   ########.fr        */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maguzman <maguzman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/24 23:31:10 by maguzman          #+#    #+#             */
+/*   Updated: 2026/05/25 12:46:36 by maguzman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft(char const *s, char (*f)(unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+	unsigned int	i;
+	char			*s2;
+	size_t			len;
+
+	i = 0;
+	if (s == NULL)
+		return (NULL);
+	len = ft_strlen(s);
+	s2 = malloc(len + 1);
+	if (s2 == NULL)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		s2[i] = f(i, s[i]);
+		i++;
+	}
+	s2[len] = '\0';
+	return (s2);
 }
 
 /*

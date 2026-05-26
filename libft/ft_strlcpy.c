d6e2d62c5d20aff0b7c83edadf269f70487985cc
maguzman <marioguzzzman@gmail.com>
2026-05-26 21:22:30 +0200

    removed guard for dest

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: maguzman <maguzman@student.42.fr>         #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/05/05 16:27:23 by maguzman         #+#    #+#              */
-/*   Updated: 2026/05/26 21:14:24 by maguzman        ###   ########.fr        */
+/*   Updated: 2026/05/26 21:22:12 by maguzman        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 
 	i = 0;
 	slen = ft_strlen(src);
-	if (size == 0 || dstsize == 0)
+	if (size == 0)
 		return (slen);
 	while (src[i] != '\0' && i < size - 1)
 	{
@@ -32,7 +32,7 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 
 /*
 DESCRIPTION
-It concatenates string with a safety guard of taking the full size of the buffer
+It copies a string with a safety guard of taking the full size of the buffer
 and making sure thee is space to NUL-temrinate the result. As long as size > 0.
 Important to make sure NUL is included in size and therefore to have size - 1.
 It operates in true "c" strings, meaning that they only take

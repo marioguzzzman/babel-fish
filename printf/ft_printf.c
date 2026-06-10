3b03ec1c9d2abb742204430a6a18fbe2563cd8ff
marioguzzzman <marioguzzman@gmail.com>
2026-06-10 21:12:50 +0200

    added c,s,d and x to selector, draft space for each function, tested ft_putchar

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,18 +6,52 @@
 /*   By: maguzman <maguzman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/09 16:56:21 by maguzman          #+#    #+#             */
-/*   Updated: 2026/06/09 20:58:08 by maguzman         ###   ########.fr       */
+/*   Updated: 2026/06/10 21:11:33 by maguzman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	print_format(char specifier, va_list ap)
+int	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+int	ft_putstr(char *str)
+{
+	write(1, &str, 1);
+	return (1);
+}
+
+int	ft_putnbr(int d, int base)
+{
+	write(1, &d, 1);
+	return (1);
+}
+
+int	ft_putunsigned(int d, int base)
+{
+	write(1, &d, 1);
+	return (1);
+}
+
+int	ft_selector(char specifier, va_list ap)
 {
 	int	count;
 
+	count = 0;
 	if (specifier == 'c')
-		print_char(va_arg(ap, char))
+		count += ft_putchar(va_arg(ap, int));
+	else if (specifier == 's')
+		count += ft_putstr(va_arg(ap, char *));
+	else if (specifier == 'd')
+		count += ft_putnbr(va_arg(ap, int), 10);
+	else if (specifier == 'x')
+		count += ft_putunsigned(va_arg(ap, unsigned int), 16);
+	else
+		count += write(1, &specifier, 1);
+	return (count);
 }
 
 int	ft_printf(const char *format, ...)
@@ -30,18 +64,23 @@ int	ft_printf(const char *format, ...)
 	while (*format != '\0')
 	{
 		if (*format == '%')
-			count += print_format(*(++format), ap);
+			count += ft_selector(*(++format), ap);
+		/*ap here is providing a pointer*/
 		else
-			count += write(1, format, 1),
+			count += write(1, format, 1);
 		++format;
 	}
 	va_end(ap);
 	return (count);
-	int	main(void)
-	{
-		int	count;
+}
 
-		count = ft_printf("hello %s?\n", "john");
-		ft_printf("the test: %d\n", count);
-		return (0);
-	}
+int	main(void)
+{
+	int	count;
+
+	count = ft_printf("hello %s\n", "john");
+	ft_printf("the test: %d\n", count);
+	ft_printf("the test: %c\n", 'j');
+	printf("hello %s\n", "john");
+	return (0);
+}

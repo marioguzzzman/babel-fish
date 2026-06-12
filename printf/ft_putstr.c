c13b5f2cc508cc4e5fbb12f5163719fdb3d6d56f
marioguzzzman <marioguzzman@gmail.com>
2026-06-12 23:11:23 +0200

    finished with main test

---
+++ printf/ft_putstr.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maguzman <maguzman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/12 22:01:18 by maguzman          #+#    #+#             */
+/*   Updated: 2026/06/12 22:11:13 by maguzman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putstr(char *str)
+{
+	int	count;
+
+	count = 0;
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+		count++;
+	}
+	return (count);
+}
+
+int	main(void)
+{
+	int	count;
+
+	write(1, "Testing ft_putstr:\n", 19);
+	count = ft_putstr("Hello, World!");
+	write(1, "\n", 1);
+	ft_printf("Characters written: %d\n", count);
+	count = ft_putstr("42");
+	write(1, "\n", 1);
+	ft_printf("Characters written: %d\n", count);
+	count = ft_putstr("");
+	write(1, "\n", 1);
+	ft_printf("Characters written for empty string: %d\n", count);
+	return (0);
+}

f51a4199cbb292bc073d448db238d75c438c6a19
marioguzzzman <marioguzzman@gmail.com>
2026-06-24 13:28:57 +0200

    added BUFFER and prototypes

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,21 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: maguzman <maguzman@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/16 10:52:48 by maguzman          #+#    #+#             */
-/*   Updated: 2026/06/16 11:00:53 by maguzman         ###   ########.fr       */
+/*                                                       :::      ::::::::    */
+/*   get_next_line.h                                   :+:      :+:    :+:    */
+/*                                                   +:+ +:+         +:+      */
+/*   By: maguzman <maguzman@student.42.fr>         #+#  +:+       +#+         */
+/*                                               +#+#+#+#+#+   +#+            */
+/*   Created: 2026/06/16 10:52:48 by maguzman         #+#    #+#              */
+/*   Updated: 2026/06/24 13:26:29 by maguzman        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
 # include <unistd.h>
 # include <stdlib.h>
 
 char	*get_next_line(int fd);
+int		ft_strlen(char *str);
+size_t	ft_strlcpy(char *dest, char *src, size_t size);
+char	*ft_strdup(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif

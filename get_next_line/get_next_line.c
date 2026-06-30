878711ce8a3a54f00db0cf0698a9fc736e58d1af
marioguzzzman <marioguzzman@gmail.com>
2026-06-30 13:13:19 +0200

    clean and organize utils and their arguments

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: maguzman <maguzman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/23 10:57:18 by maguzman          #+#    #+#             */
-/*   Updated: 2026/06/27 01:46:35 by maguzman         ###   ########.fr       */
+/*   Updated: 2026/06/30 12:49:35 by maguzman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,10 @@ char	*get_next_line(int fd)
 		return (NULL);
 	b_read = read(fd, buf, BUFFER_SIZE);
 	if (b_read == -1)
+	{
+		left_o = NULL;
 		return (NULL);
+	}
 	if (b_read == 0)
 	{
 		if (left_o == NULL)
@@ -83,32 +86,4 @@ char	*get_next_line(int fd)
 	line = left_o;
 	left_o = NULL;
 	return (line);
-}
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*ptr;
-	size_t	copy_len;
-	size_t	s_len;
-
-	if (s == NULL)
-		return (NULL);
-	s_len = ft_strlen(s);
-	if (start > s_len)
-	{
-		ptr = malloc(1);
-		if (ptr == NULL)
-			return (NULL);
-		ptr[0] = '\0';
-		return (ptr);
-	}
-	if (s_len - start < len)
-		copy_len = s_len - start;
-	else
-		copy_len = len;
-	ptr = malloc(copy_len + 1);
-	if (ptr == NULL)
-		return (NULL);
-	ft_strlcpy(ptr, s + start, copy_len + 1);
-	return (ptr);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                       :::      ::::::::    */
-/*   get_next_line.h                                   :+:      :+:    :+:    */
-/*                                                   +:+ +:+         +:+      */
-/*   By: maguzman <maguzman@student.42.fr>         #+#  +:+       +#+         */
-/*                                               +#+#+#+#+#+   +#+            */
-/*   Created: 2026/06/16 10:52:48 by maguzman         #+#    #+#              */
-/*   Updated: 2026/06/24 13:26:29 by maguzman        ###   ########.fr        */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maguzman <maguzman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/16 10:52:48 by maguzman          #+#    #+#             */
+/*   Updated: 2026/06/30 12:55:17 by maguzman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@
 # include <stdlib.h>
 
 char	*get_next_line(int fd);
-int		ft_strlen(char *str);
-size_t	ft_strlcpy(char *dest, char *src, size_t size);
-char	*ft_strdup(const char *s);
+int		ft_strlen(const char *str);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
